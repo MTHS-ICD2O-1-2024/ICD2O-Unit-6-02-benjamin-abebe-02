c094a5c7aad467b7986a4119b5cea6e0bb7bbc53
benjamin-abebe <150177783+benjamin-abebe-02@users.noreply.github.com>
2025-05-14 17:29:13 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// 
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+let numberOfCookies = 0
+document.getElementById("answer").innerHTML = "<p>You have no cookies right now! Click the button to get as many as you want!</p>"
+
+function myButtonClicked() {
+  numberOfCookies = numberOfCookies + 1
+  document.getElementById("answer").innerHTML = "<p>You have " + numberOfCookies + " Cookies</p>"
+}
+
