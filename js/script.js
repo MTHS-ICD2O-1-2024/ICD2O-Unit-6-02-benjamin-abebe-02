4bbe2122b4610a35b034898bbd31195ad5a47206
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-23 17:26:53 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -7,10 +7,20 @@
 "use strict"
 
 let numberOfCookies = 0
-document.getElementById("answer").innerHTML = "<p>You have no cookies right now! Click the button to get as many as you want!</p>"
+
+
+if (localStorage.getItem("savedCookies")) {
+  numberOfCookies = parseInt(localStorage.getItem("savedCookies"))
+  document.getElementById("answer").innerHTML =
+    "<p> You previously had " + numberOfCookies + " cookies.</p>"
+} else {
+  document.getElementById("answer").innerHTML =
+    "<p>You have no cookies right now! Click the button to get as many as you want!</p>"
+}
 
 function myButtonClicked() {
   numberOfCookies = numberOfCookies + 1
-  document.getElementById("answer").innerHTML = "<p>You have " + numberOfCookies + " Cookies</p>"
+  localStorage.setItem("savedCookies", numberOfCookies)
+  document.getElementById("answer").innerHTML =
+    "<p>You have " + numberOfCookies + " Cookies</p>"
 }
-
